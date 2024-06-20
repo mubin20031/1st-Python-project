a7acaf456a70bd637c792dd8e74f8384552c982b
mubin20031 <148771435+mubin20031@users.noreply.github.com>
2024-06-21 01:19:19 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,8 @@
+#My first code in Python adding two numbers and storing it on another variable called c
+
+print("Enter the value for a: ")
+a = int(input())
+print("Enter the value for b: ")
+b = int(input())
+c = a + b
+print("The addition is: ",c)
